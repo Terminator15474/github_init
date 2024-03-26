676492f4d5918e0e80c7b4f3e47dae12182cea0b
Terminator15474 <jandommert8@gmail.com>
2024-03-26 23:46:52 +0100

    added option to create private repo

--- gh-init.bat
+++ gh-init.bat
@@ -2,10 +2,32 @@
 for %%I in (.) do set name=%%~nxI
 echo %name%
 
+SETLOCAL privateFlagSet=0
+
+:processArgs
+SET arg=%1
+IF DEFINED arg (
+	IF %arg%=="-p" (
+		SET privateFlagSet=1
+	)
+	IF %arg%=="--private" (
+		SET privateFlagSet=1
+	)
+
+	shift
+	GOTO processArgs
+)
+
 git init
 git add *
 git commit -m "automated creation of repo %name%"
 git branch -M main
-gh repo create %name% --public
+
+IF %privateFlagSet%==1 (
+	gh repo create %name% --private
+) ELSE (
+	gh repo create %name% --public
+)
+
 git remote add origin https://github.com/Terminator15474/%name%.git
 git push -u origin main
