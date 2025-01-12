52218d5794f62116cb1492b88eb8760ccb407961
Terminator15474 <jandommert8@gmail.com>
2025-01-12 22:55:59 +0100

    added quotes around project name

--- gh-init.bat
+++ gh-init.bat
@@ -1,12 +1,14 @@
 @echo off
+SET privateFlagSet=0
+
 for %%I in (.) do set name=%%~nxI
 echo %name%
 
-SETLOCAL privateFlagSet=0
+IF "%1"=="" GOTO :createRepo
 
 :processArgs
 SET arg=%1
-IF DEFINED arg (
+IF DEFINED !arg! (
 	IF %arg%=="-p" (
 		SET privateFlagSet=1
 	)
@@ -16,11 +18,13 @@ IF DEFINED arg (
 
 	shift
 	GOTO processArgs
-)
+) 
+
+:createRepo
 
 git init
 git add *
-git commit -m "automated creation of repo %name%"
+git commit -m "automated creation of repo '%name%'"
 git branch -M main
 
 IF %privateFlagSet%==1 (
