640c6def7ab6cb85dd21dfd458b5914cdd07fe8a
Terminator15474 <jandommert8@gmail.com>
2025-01-26 20:17:14 +0100

    Finally Fixed private repo creation

--- gh-init.bat
+++ gh-init.bat
@@ -1,4 +1,5 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
 SET privateFlagSet=0
 
 for %%I in (.) do set name=%%~nxI
@@ -8,30 +9,29 @@ IF "%1"=="" GOTO :createRepo
 
 :processArgs
 SET arg=%1
-IF DEFINED !arg! (
-	IF %arg%=="-p" (
-		SET privateFlagSet=1
-	)
-	IF %arg%=="--private" (
-		SET privateFlagSet=1
-	)
-
-	shift
-	GOTO processArgs
-) 
+echo Processing arguement : "!arg!"
+IF "!arg!"=="-p" (
+	SET privateFlagSet=1
+)
+IF "!arg!"=="--private" (
+	SET privateFlagSet=1
+)
+shift
+IF NOT "!arg!"=="" GOTO processArgs
 
 :createRepo
 
-git init
-git add *
-git commit -m "automated creation of repo '%name%'"
-git branch -M main
-
-IF %privateFlagSet%==1 (
+rem git init
+rem git add *
+rem git commit -m "automated creation of repo '%name%'"
+rem git branch -M main
+echo !privateFlagSet!
+IF !privateFlagSet! == 1 (
+	echo Creating a privvate repo
 	gh repo create %name% --private
 ) ELSE (
 	gh repo create %name% --public
 )
 
-git remote add origin https://github.com/Terminator15474/%name%.git
-git push -u origin main
+rem git remote add origin https://github.com/Terminator15474/%name%.git
+rem git push -u origin main
